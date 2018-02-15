9dd090e08f074ae74326d3812132edc40929905d
Bartłomiej Olender <OlenderBartlomiej00@gmail.com>
2018-02-15 21:17:00 +0100

    Generate name.

--- 00. Name Generator/main.py
+++ 00. Name Generator/main.py
@@ -3,8 +3,15 @@ vowels = ['a', 'e', 'i', 'o', 'u', 'y']
 letters = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
 
 def main():
-    for x in range(0, 50):
-        print(GenerateSyllable())
+    fullName = GenerateName() + " "
+    #fullName += GenerateLastName()
+    print(fullName)
+
+def GenerateName():
+    name = ''
+    for x in range(randint(1, 4)):
+        name += GenerateSyllable()
+    return name
 
 def GenerateSyllable():
     syllable = ''
@@ -16,8 +23,8 @@ def GenerateSyllable():
             syllable += RandomLetter(False)
     return syllable
 
-def RandomLetter(vowel):
-    if vowel == True:
+def RandomLetter(isVowel):
+    if isVowel == True:
         return vowels[randint(0,len(vowels) - 1)]
     else:
         return letters[randint(0,len(letters) - 1)]
